abe7312b31b0d7a23fcbe3645844826c55fb3780
retif <retifrav@gmail.com>
2022-01-02 16:58:49 +0100

    About window
    - added About window
    - refined the menu

--- src/tap_adql_sandbox/__main__.py
+++ src/tap_adql_sandbox/__main__.py
@@ -40,22 +40,21 @@ def main():
     # dpg.set_viewport_resize_callback(callback=backend.updateGeometry)
     dpg.set_exit_callback(callback=lambda: dpg.save_init_file(settingsFile))
 
+    #
+    # --- main window
+    #
     with dpg.window(
         tag=mainWindowID
         # width=900,
         # height=500,
-        # no_scrollbar=False  # FIXME doesn't seem to work
+        # no_scrollbar=True  # FIXME doesn't seem to work
     ):
         #
         # --- menu
         #
         with dpg.menu_bar():
             with dpg.menu(label="File"):
-                dpg.add_menu_item(label="Save", callback=lambda: print("ololo"))
-
-                with dpg.menu(label="Settings"):
-                    dpg.add_menu_item(label="Setting 1", callback=lambda: print("ololo"), check=True)
-                    dpg.add_menu_item(label="Setting 2", callback=lambda: print("ololo"))
+                dpg.add_menu_item(label="Exit", callback=lambda: dpg.stop_dearpygui())
 
             if cliArgs.debug:
                 with dpg.menu(label="Dev"):
@@ -72,8 +71,23 @@ def main():
                         callback=lambda: dpg.show_imgui_demo()
                     )
 
-            with dpg.menu(label="About"):
-                dpg.add_menu_item(label="DearPyGui", callback=lambda: dpg.show_about())
+            with dpg.menu(label="Help"):
+                with dpg.menu(label="Examples"):
+                    dpg.add_menu_item(
+                        label="Query to exoplanet.eu",
+                        callback=lambda: print("ololo")
+                    )
+                    dpg.add_menu_item(
+                        label="Query to NASA",
+                        callback=lambda: print("ololo")
+                    )
+                dpg.add_spacer()
+                dpg.add_separator()
+                dpg.add_spacer()
+                dpg.add_menu_item(
+                    label="About...",
+                    callback=lambda: dpg.configure_item("about", show=True)
+                )
         #
         # -- contents
         #
@@ -88,6 +102,44 @@ def main():
         dpg.add_button(label="Execute query", callback=lambda: print("ololo"))
 
         dpg.add_text("Query results")
+    #
+    # --- about window
+    #
+    with dpg.window(
+        tag="about",
+        label="About application",
+        modal=True,
+        show=False,
+        no_title_bar=False
+        # no_scrollbar=True
+    ):
+        dpg.add_text(f"Version: {__version__}")
+        dpg.add_text(
+            " ".join((
+                "Description: A sandbox application for executing",
+                "ADQL queries\nvia TAP interface of various data sources,",
+                "such as astronomical\ndatabases.",
+                "Essentially, it's just a GUI for PyVO."
+            ))
+        )
+        dpg.add_text(f"Source code: https://github.com/retifrav/tap-adql-sandbox")
+        dpg.add_text(f"License: GPLv3")
+        dpg.add_spacer()
+        dpg.add_separator()
+        dpg.add_spacer(height=5)
+        with dpg.group(horizontal=True):
+            dpg.add_text("Created with DearPyGui")
+            dpg.add_button(
+                label="about that...",
+                callback=lambda: dpg.show_about()
+            )
+        dpg.add_spacer(height=5)
+        dpg.add_separator()
+        dpg.add_spacer(height=10)
+        dpg.add_button(
+            label="Close",
+            callback=lambda: dpg.configure_item("about", show=False)
+        )
 
     setTheme()
 
@@ -121,8 +173,6 @@ def main():
         # dpg.stop_dearpygui()
 
     dpg.destroy_context()
-
-    raise SystemExit(0)
 
 
 if __name__ == '__main__':
